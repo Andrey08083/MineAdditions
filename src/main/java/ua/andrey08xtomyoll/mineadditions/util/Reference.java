cc2b0a3837bb4531400e838d2f17447cd0c10391
Andrey_08 <labatiy08@gmail.com>
2021-01-26 18:40:26 +0200

    Fix proxy classpaths

--- src/main/java/ua/andrey08xtomyoll/mineadditions/util/Reference.java
+++ src/main/java/ua/andrey08xtomyoll/mineadditions/util/Reference.java
@@ -6,6 +6,6 @@ public class Reference
     public static final String NAME = "MineAdditions";
     public static final String VERSION = "1.0";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-    public static final String CLIENT_PROXY_CLASS = "ua.andrey08.mineadditions.proxy.ClientProxy";
-    public static final String COMMON_PROXY_CLASS = "ua.andrey08.mineadditions.proxy.CommonProxy";
+    public static final String CLIENT_PROXY_CLASS = "ua.andrey08xtomyoll.mineadditions.proxy.ClientProxy";
+    public static final String COMMON_PROXY_CLASS = "ua.andrey08xtomyoll.mineadditions.proxy.CommonProxy";
 }
